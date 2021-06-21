278bd930ce2eab890b5f046f529185913e8ca576
xiangxu8381 <40554300+xiangxu8381@users.noreply.github.com>
2021-06-21 16:59:11 +0800

    Create MyMessage.java

---
+++ src/main/java/com/example/demo/po/MyMessage.java
@@ -0,0 +1,13 @@
+package com.example.demo.po;
+
+import lombok.Data;
+
+/**
+ * @Author : xiangxu
+ * @Date : 2021-06-21 16:22
+ **/
+@Data
+public class MyMessage {
+    private  String userId;
+    private String message;
+}
