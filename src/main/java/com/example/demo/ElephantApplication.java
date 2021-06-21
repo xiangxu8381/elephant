d2f0d0e73b583b981bfbe8a0b40b73dadc9c34e5
xiangxu8381 <40554300+xiangxu8381@users.noreply.github.com>
2021-06-21 16:59:51 +0800

    Create ElephantApplication.java

---
+++ src/main/java/com/example/demo/ElephantApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElephantApplication.class, args);
+    }
+
+}
