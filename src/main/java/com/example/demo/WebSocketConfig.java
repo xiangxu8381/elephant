05f105d61b75ce981f443169063bae07693210c0
xiangxu8381 <40554300+xiangxu8381@users.noreply.github.com>
2021-06-21 16:56:05 +0800

    Create WebSocketConfig.java

---
+++ src/main/java/com/example/demo/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Author : xiangxu
+ * @Date : 2021-06-21 11:01
+ **/
+@Component
+public class WebSocketConfig {
+    /**
+     * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
